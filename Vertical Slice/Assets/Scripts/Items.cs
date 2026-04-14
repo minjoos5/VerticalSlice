51ffeae6fc3c6d8ed97ecc0d0689561767ec7f08
Minjoo <minjoos5@uci.edu>
2026-04-13 21:24:16 -0700

    added run mode
    space to run
    added item script (going to override)

---
+++ Vertical Slice/Assets/Scripts/Items.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    // the list of items: key, cassette tape, cassette tape;
+    // the item disappears when the player picks it up
+    // maybe interactable with E key?
+    // shift to run
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- Vertical Slice/Assets/Scripts/Player.cs
+++ Vertical Slice/Assets/Scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float _speed = 5f;
+
+    [SerializeField] private float _runSpeed = 10f;
+    [SerializeField] private float _mouseSensitivity = 100f;
+
+    private Transform _cameraTransform;
+    private float _rotationX = 0f;
+
     void Start()
     {
-        
+        // freeze the cursor in the middle of the screen and hide it
+        // instead of the cursor, the crosshair will be used to aim
+        _cameraTransform = Camera.main.transform;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Mouse movement
+        float _xMouse = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+        float _yMouse = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * _xMouse);
+
+        _rotationX -= _yMouse;
+        _rotationX = Mathf.Clamp(_rotationX, -90f, 90f);
+        _cameraTransform.localRotation = Quaternion.Euler(_rotationX, 0f, 0f);
+    
+        // WASD movement of player
+        float _xInput = Input.GetAxis("Horizontal");
+        float _zInput = Input.GetAxis("Vertical");
+
+        Vector3 _playerMovement = transform.right * _xInput + transform.forward * _zInput;
+        transform.position += _playerMovement * _speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += _playerMovement * _runSpeed * Time.deltaTime;
+        }
     }
+
 }
