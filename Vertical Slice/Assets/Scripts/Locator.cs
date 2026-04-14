27313baacf6df4af1eb8100eea78569773372d31
Minjoo <minjoos5@uci.edu>
2026-04-13 22:57:12 -0700

    created NPC script and Locator script

---
+++ Vertical Slice/Assets/Scripts/Locator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Locator : MonoBehaviour
+{
+    public static Locator Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy (this);
+            return;
+        }
+
+        Instance = this;   
+    }
+
+    public Player _player;
+    public Items _items;
+    public NPC _NPC;
+
+}
