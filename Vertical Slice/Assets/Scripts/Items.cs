90449f909c6b42f03d12e1a868d35908758fc092
Minjoo <minjoos5@uci.edu>
2026-04-14 00:09:30 -0700

    updated command button

--- Vertical Slice/Assets/Scripts/Items.cs
+++ Vertical Slice/Assets/Scripts/Items.cs
@@ -7,7 +7,7 @@ public class Items : MonoBehaviour
     // the list of items: key, cassette tape, cassette tape;
     // the item disappears when the player picks it up
     // maybe interactable with E key?
-    // shift to run
+    // space to run
     void Start()
     {
         
