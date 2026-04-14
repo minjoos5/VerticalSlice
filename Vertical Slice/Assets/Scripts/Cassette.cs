a812181b75259301b043d5b7cfdd59ed0b59a5b7
Minjoo <minjoos5@uci.edu>
2026-04-14 00:16:53 -0700

    added item manager game object

--- Vertical Slice/Assets/Scripts/Cassette.cs
+++ Vertical Slice/Assets/Scripts/Cassette.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class Cassette : Items
 {
+    [SerializeField] GameObject _fakeCas1;
+    [SerializeField] GameObject _fakeCas2;
+    [SerializeField] GameObject _trueCas;
 
+    
 }
