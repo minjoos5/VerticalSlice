3d31f0067275373009490c4449f27abf6b8da74c
Minjoo <minjoos5@uci.edu>
2026-04-14 00:53:44 -0700

    added state machine to NPC

--- Vertical Slice/Assets/Scripts/NPC.cs
+++ Vertical Slice/Assets/Scripts/NPC.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum NPCstate
+    {
+        Walking,
+        Chasing,
+        Attacking
+    }
+
 public class NPC : MonoBehaviour
 {
     [SerializeField] private Transform target;
     private NavMeshAgent agent;
+
+    private NPCstate _currentActivity;
     private void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
