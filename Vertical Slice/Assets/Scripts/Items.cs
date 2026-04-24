dae8a997bab28a254905c3c42e0a2e9cf9624569
Minjoo <minjoos5@uci.edu>
2026-04-24 00:16:06 -0700

    E to interact works

--- Vertical Slice/Assets/Scripts/Items.cs
+++ Vertical Slice/Assets/Scripts/Items.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+interface Interactable
+{
+    public void Interact();
+}
 public class Items : MonoBehaviour
 {
     // the list of items: key, cassette tape, cassette tape;
     // the item disappears when the player picks it up
     // maybe interactable with E key?
     // space to run
-
     [SerializeField] List <GameObject> _location = new List <GameObject> {};
+    
+    [SerializeField] Transform _playerTransform;
+
+    public float _interact;
+
+
     void Start()
     {
         foreach (GameObject _spot in _location)
@@ -18,15 +28,30 @@ public class Items : MonoBehaviour
         }
     }
 
-    
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            InteractionE();
+            Debug.Log("interacting now");
+        }
+    }
+
     public void OnMouseDown()
     {
         Debug.Log("clicked!");
         gameObject.SetActive(false);
     }
 
-    public void Interaction()
+    public void InteractionE()
     {
-        
+        Ray _ray = new Ray (_playerTransform.position, _playerTransform.forward);
+        if (Physics.Raycast(_ray, out RaycastHit _hit, _interact))
+        {
+            if (_hit.collider.gameObject.TryGetComponent(out Interactable _targetObj))
+            {
+                _targetObj.Interact();
+            }
+        }
     }
 }
