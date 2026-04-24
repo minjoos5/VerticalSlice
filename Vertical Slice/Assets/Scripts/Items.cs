62619a76afb7624ba2dbdc8b454da4fae4f426d1
Minjoo <minjoos5@uci.edu>
2026-04-24 01:02:05 -0700

    wrote a code for random generator

--- Vertical Slice/Assets/Scripts/Items.cs
+++ Vertical Slice/Assets/Scripts/Items.cs
@@ -14,27 +14,53 @@ public class Items : MonoBehaviour
     // maybe interactable with E key?
     // space to run
     [SerializeField] List <GameObject> _location = new List <GameObject> {};
+
+    [SerializeField] List <GameObject> _items = new List <GameObject> {};
     
     [SerializeField] Transform _playerTransform;
+
+    private List <int> _temp = new List <int> {};
 
     public float _interact;
 
 
-    void Start()
+    int r = 0;
+
+
+
+    void Awake()
     {
         foreach (GameObject _spot in _location)
         {
             _spot.SetActive(true);
         }
+
+        foreach (GameObject _spot in _items)
+        {
+            _spot.SetActive(false);
+        }
+
+    }
+    void Start()
+    {
+
+        for (int i = 0; i < _location.Count; i++)
+        {
+            r = Random.Range(0, _location.Count - 1);
+            _items[i].transform.position = _location[r].transform.position;
+            _location.RemoveAt(r);
+        }
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        /*if (Input.GetKeyDown(KeyCode.E))
         {
             InteractionE();
             Debug.Log("interacting now");
-        }
+        }*/
+
+
     }
 
     public void OnMouseDown()
@@ -43,7 +69,7 @@ public class Items : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    public void InteractionE()
+    /*public void InteractionE()
     {
         Ray _ray = new Ray (_playerTransform.position, _playerTransform.forward);
         if (Physics.Raycast(_ray, out RaycastHit _hit, _interact))
@@ -53,5 +79,5 @@ public class Items : MonoBehaviour
                 _targetObj.Interact();
             }
         }
-    }
+    }*/
 }
