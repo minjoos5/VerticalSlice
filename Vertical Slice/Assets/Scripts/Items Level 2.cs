182551a10b50006cfbe550649898c4015dfd6f9f
Minjoo <minjoos5@uci.edu>
2026-05-25 18:42:48 -0700

    working on level 2 items

---
+++ Vertical Slice/Assets/Scripts/Items Level 2.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Items : MonoBehaviour
+{
+    // the list of items: key, cassette tape, cassette tape;
+    // the item disappears when the player picks it up
+    // maybe interactable with E key?
+    // space to run
+    [SerializeField] public List <GameObject> _location = new List <GameObject> {};
+    [SerializeField] public List <GameObject> _items = new List <GameObject> {};
+    //[SerializeField] 
+    public Transform _playerTransform;
+    public Camera _camera;
+
+    public List <GameObject> _temp = new List <GameObject> {};
+
+    public float _interact = 10.0f;
+
+    void Awake()
+    {
+
+        //_playerTransform = GameObject.Find("Player Capsule").transform;
+
+        foreach (GameObject _spot in _location)
+        {
+            _spot.SetActive(true);
+        }
+
+        foreach (GameObject _spot in _items)
+        {
+            _spot.SetActive(false);
+        }
+        
+        _temp = _location.OrderBy( x => Random.value ).ToList( );
+        for (int i = 0; i < _location.Count; i++)
+        {
+            _items[i].transform.position = _temp[i].transform.position;
+            _items[i].SetActive(true);
+        }
+    }
+
+    public virtual void OnMouseDown()
+    {
+        Debug.Log("clicked!");
+        gameObject.SetActive(false);
+    }
+
+        /*public void Update()
+    {
+        InheritUpdate();
+    }
+
+    public virtual void InheritUpdate()
+    {
+        Locator.Instance._player.InteractionE_Cassette();
+        Locator.Instance._player.InteractionE_Door();
+        Locator.Instance._knife.InteractionE_Knife();
+    }*/
+}
